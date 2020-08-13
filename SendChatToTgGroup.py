990a816cf461ed12940ffdeb817b5e5b0122efdf
hank9999 <hank9999@qq.com>
2020-08-13 18:33:02 +0800

    ClientTimeout fix

--- SendChatToTgGroup.py
+++ SendChatToTgGroup.py
@@ -4,7 +4,7 @@ from setting import tg_setting
 
 async def sendMessage(name, perm, text, server):
     data = {'name': name, 'perm': perm, 'text': text, 'server': server}
-    timeout = aiohttp.ClientTimeout(total=1)
+    timeout = aiohttp.ClientTimeout(total=10)
     async with aiohttp.ClientSession(timeout=timeout) as session:
         async with session.post(tg_setting['url'], data=data):
             pass
